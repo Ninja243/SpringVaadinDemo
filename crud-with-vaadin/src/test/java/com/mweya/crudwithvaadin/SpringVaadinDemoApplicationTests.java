d3e26c88d31e248b2a7cd98a4695eb54c5028f4f
Ninja243 <mweyaruider@gmail.com>
2020-04-13 12:55:12 +0200

    Done

---
+++ crud-with-vaadin/src/test/java/com/mweya/crudwithvaadin/SpringVaadinDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mweya.crudwithvaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVaadinDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
